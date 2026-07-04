20b3dc89033c5f6b7c5090fc6c8981120d82d817
Mit Gajera <gajeramit180@gmail.com>
2026-07-04 12:04:55 +0530

    feat(sdk): add TxLINE feed client and Proxa program client

---
+++ packages/sdk/src/index.ts
@@ -0,0 +1,8 @@
+export * from "./constants";
+export * from "./pdas";
+export * from "./accounts";
+export * from "./resolve";
+export * from "./txline";
+export * from "./client";
+export type { Proxa } from "./idl/proxa";
+export { default as PROXA_IDL } from "./idl/proxa.json";
